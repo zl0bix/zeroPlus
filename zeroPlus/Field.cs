d89e7fcba6123caea4b98df1db12e62c06220318
Andrew <zl0bix@icloud.com>
2024-12-15 12:55:59 +0500

    правка

--- zeroPlus/Field.cs
+++ zeroPlus/Field.cs
@@ -9,7 +9,8 @@ namespace zeroPlus
 {
     internal class Field
     {
-        public void SetArr(ref char[,] arr, int num, char ch)
+        Random _rnd = new Random();
+        public bool SetArr(ref char[,] arr, int num, char ch)
         {
             //[0,0],[0,1],[0,2]
             //[1,0],[1,1],[1,2]
@@ -17,43 +18,89 @@ namespace zeroPlus
             
                 switch(num)
                 {
-                    case 1:
-                         arr[0, 0] = ch;
-                         break;
+                case 1:
+                    if (arr[0, 0] != 'O' || arr[0, 0] != 'X')
+                    {
+                        arr[0, 0] = ch;
+
+                        return true;
+                    }
+                    return false;  
                         
-                    case 2:
+                case 2:
+                    if (arr[0, 1] != 'O' || arr[0, 1] != 'X')
+                    {
                         arr[0, 1] = ch;
-                        break;
+
+                        return true;
+                    }
+                    return false;
 
                 case 3:
+                    if (arr[0, 2] != 'O' || arr[0, 2] != 'X')
+                    {
                         arr[0, 2] = ch;
-                        break;
+
+                        return true;
+                    }
+                    return false;
 
                 case 4:
+                    if (arr[1, 0] != 'O' || arr[1, 0] != 'X')
+                    {
                         arr[1, 0] = ch;
-                        break;
+
+                        return true;
+                    }
+                    return false;
                 case 5:
+                    if (arr[1, 1] != 'O' || arr[1, 1] != 'X')
+                    {
                         arr[1, 1] = ch;
-                        break;
+
+                        return true;
+                    }
+                    return false;
 
                 case 6:
+                    if (arr[1, 2] != 'O' || arr[1, 2] != 'X')
+                    {
                         arr[1, 2] = ch;
-                        break;
+
+                        return true;
+                    }
+                    return false;
 
                 case 7:
+                    if (arr[2, 0] != 'O' || arr[2, 0] != 'X')
+                    {
                         arr[2, 0] = ch;
-                        break;
+
+                        return true;
+                    }
+                    return false;
 
                 case 8:
+                    if (arr[2, 1] != 'O' || arr[2, 1] != 'X')
+                    {
                         arr[2, 1] = ch;
-                        break;
+
+                        return true;
+                    }
+                    return false;
 
                 case 9:
+                    if (arr[2, 2] != 'O' || arr[2, 2] != 'X')
+                    {
                         arr[2, 2] = ch;
-                        break;
+
+                        return true;
+                    }
+                    return false;
 
                 default:
-                        break;
+
+                        return false;
             }                         
         }
 
@@ -81,10 +128,65 @@ namespace zeroPlus
 
         public bool IsWin(ref char[,] arr , char ch)
         {
-            if (arr[0,1] == ch && arr[0,2] == ch)
+            if (arr[0, 0] == ch && arr[0, 1] == ch && arr[0, 2] == ch)
+            {
+                Console.WriteLine("Выиграл игрок игравший за " + ch);
+                return true;
+            }
+            else if (arr[1, 1] == ch && arr[1, 1] == ch && arr[1, 2] == ch)
+            {
+                Console.WriteLine("Выиграл игрок игравший за " + ch);
+                return true;
+            }
+            else if (arr[2, 1] == ch && arr[2, 1] == ch && arr[2, 2] == ch)
+            {
+                Console.WriteLine("Выиграл игрок игравший за " + ch);
+                return true;
+            }
+            else if (arr[0, 0] == ch && arr[1, 0] == ch && arr[2, 0] == ch)
+            {
+                Console.WriteLine("Выиграл игрок игравший за " + ch);
+                return true;
+            }
+            else if (arr[1, 0] == ch && arr[1, 1] == ch && arr[1, 2] == ch)
+            {
+                Console.WriteLine("Выиграл игрок игравший за " + ch);
+                return true;
+            }
+            else if (arr[2, 0] == ch && arr[2, 1] == ch && arr[2, 2] == ch)
+            {
+                Console.WriteLine("Выиграл игрок игравший за " + ch);
+                return true;
+            }
+            else if (arr[0, 0] == ch && arr[1, 1] == ch && arr[2, 2] == ch)
+            {
+                Console.WriteLine("Выиграл игрок игравший за " + ch);
+                return true;
+            }
+            else if (arr[2, 0] == ch && arr[1, 1] == ch && arr[0, 2] == ch)
+            {
+                Console.WriteLine("Выиграл игрок игравший за " + ch);
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+        public bool OpponenеMove(ref char[,] arr, char ch)
+        {
+            int numX = _rnd.Next(0, 3);
+            int numY = _rnd.Next(0, 3);
+            if (arr[numX, numY] != ch && arr[numX, numY] != 'O')
+            {
+                arr[numX, numY] = 'O';
 
                 return true;
-            else if()
+            }
+            else
+
+               return false;
+
         }
     }
 }
--- zeroPlus/Program.cs
+++ zeroPlus/Program.cs
@@ -18,18 +18,61 @@ namespace zeroPlus
                     {'.','.','.' }
                 };
             int number;
-            do
-            {
-                Console.Write("Введите номер поля -> ");
 
-                if(int.TryParse(Console.ReadLine(), out number))
+            bool isWin = false;
 
-                field.SetArr(ref chars, number,'X');
+            while (!isWin)
+            {   
+                Console.Clear();
+                field.DrowField(ref chars);
+                //do
+                Console.Write("\n1 2 3\n4 5 6\n7 8 9\nВведите номер поля согласно схеме -> ");
+
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+
+                    field.SetArr(ref chars, number, 'X');
+                }
+                while (!field.SetArr(ref chars, number, 'X'))
+                {
+                    Console.Write("\n1 2 3\n4 5 6\n7 8 9\nВведите номер поля согласно схеме -> ");
+
+                    if (int.TryParse(Console.ReadLine(), out number))
+                    {
+
+                        field.SetArr(ref chars, number, 'X');
+                    }
+                    Console.Clear();
+                    field.DrowField(ref chars);
+                }
+
+                if (field.IsWin(ref chars, 'X'))
+                {
+                    isWin = true;
+
+                    continue;
+                }
+                    while(!field.OpponenеMove(ref chars,'X'))
+                    {
+                        field.OpponenеMove(ref chars, 'X');
+                        
+                    }
+
+                if (field.IsWin(ref chars, 'O'))
+                {
+                    isWin = true;
+
+                    continue;
+                }
+
+
+
+                //while (!isWin);
             }
-            while (!field.ChekArr(number));
+
             
-           
             field.DrowField(ref chars);
+            Console.WriteLine("Молодец!!!");            
         }
     }
 }
